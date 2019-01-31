7781d34809990411a5051fb3e83c57c83fa25e44
samirthaker2020 <46947688+samirthaker2020@users.noreply.github.com>
2019-01-30 20:35:52 -0500

    modify

--- src/New/StringFunction.java
+++ src/New/StringFunction.java
@@ -4,7 +4,7 @@ package New;
 public class StringFunction {
 
     public static void main(String[] args) {
-        String string = "abcdef";
+        String string = "abcdeffdjvfnjvn";
         String reverse = new StringBuffer(string).reverse().toString();
         System.out.println("\nString before reverse: "+string);
         System.out.println("String after reverse: "+reverse);
