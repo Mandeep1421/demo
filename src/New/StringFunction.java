dad0e3af250f989e8aff2a05f1c58b85ea9470e6
Mandeep1421 <Mandeep1421@users.noreply.github.com>
2019-01-30 19:47:26 -0500

    Java Code

--- src/New/StringFunction.java
+++ src/New/StringFunction.java
@@ -1,4 +1,13 @@
 package New;
 
+
 public class StringFunction {
-}
+
+    public static void main(String[] args) {
+        String string = "abcdef";
+        String reverse = new StringBuffer(string).reverse().toString();
+        System.out.println("\nString before reverse: "+string);
+        System.out.println("String after reverse: "+reverse);
+    }
+    }
+
