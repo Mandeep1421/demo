fd97c8bf4b67f8da6d97e169105bd31dd46b846c
Mandeep1421 <Mandeep1421@users.noreply.github.com>
2019-01-30 20:47:59 -0500

    test

---
+++ StringHandling/src/New/StringIndex.java
@@ -0,0 +1,11 @@
+package New;
+
+public class StringIndex {
+    public void index(){
+    String str_Sample = "RockStar";
+System.out.println("Character at position 5: " + str_Sample.charAt(5));
+System.out.println("Index of character 'S': " + str_Sample.indexOf('S'));
+System.out.println("Length of String: " + str_Sample.length());
+System.out.println("Contains sequence 'tar': " + str_Sample.contains("ock"));
+    }
+}
